0553bb3add4b2282c99d44592c28eff6c312bf8f
marcinbogiel <marcin.bogiel@gmail.com>
2020-03-11 19:19:20 +0100

    Compiled example for openMP

---
+++ hello_openmp.c
@@ -0,0 +1,11 @@
+#include <omp.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+int main(int argc, char **argv)
+{ 
+    #pragma omp parallel 
+    { 
+        printf("Hello from thread = %d\n", omp_get_thread_num()); 
+    } 
+} 
