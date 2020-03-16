e6d9d379ad8ada0b979770d0bafdaf03c61a719f
marcinbogiel <marcin.bogiel@gmail.com>
2020-03-16 18:56:50 +0100

    Second version of solution

--- Course solutions/exercise_2_v2.c
+++ Course solutions/exercise_2_v2.c
@@ -11,12 +11,13 @@
   
 static long num_steps = 100000;
 double step; 
+#define PAD 8
 #define NUM_THREADS 2
 
 int main(int argc, char **argv)
 { 
     int i, nthreads; 
-    double x, pi, sum[NUM_THREADS];
+    double x, pi, sum[NUM_THREADS][PAD];
 
     step = 1.0/(double) num_steps;
     omp_set_num_threads(NUM_THREADS);
@@ -34,14 +35,14 @@ int main(int argc, char **argv)
         if(id==0)
             nthreads = nthrds;
         
-        for(i=id, sum[id]=0.0; i<num_steps; i=i+nthrds){
+        for(i=id, sum[id][0]=0.0; i<num_steps; i=i+nthrds){
             x = (i+0.5)*step;
-            sum[id] += 4.0/(1.0+x*x);
+            sum[id][0] += 4.0/(1.0+x*x);
         }
     }
 
     for(i=0, pi=0.0; i<nthreads;i++)
-        pi += sum[i] * step;
+        pi += sum[i][0] * step;
 
     double end_time = omp_get_wtime();
 
