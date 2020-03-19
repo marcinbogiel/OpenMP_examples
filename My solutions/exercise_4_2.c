8556e2573de0d1393537024a0d441f891d5b1a50
marcinbogiel <marcin.bogiel@gmail.com>
2020-03-19 19:18:52 +0100

    Picking schema works

--- My solutions/exercise_4_2.c
+++ My solutions/exercise_4_2.c
@@ -14,6 +14,8 @@
 static long num_steps = 100000;
 double step; 
 
+omp_sched_t get_schema_of_loop();
+
 int main(int argc, char **argv)
 { 
     int i;
@@ -23,9 +25,11 @@ int main(int argc, char **argv)
 
     double start_time = omp_get_wtime();
 
+    //printf("Maximum amount of threads for this machine: %d\n",omp_get_max_threads());
+    printf("\n4 flaga\n");
     omp_set_num_threads(4);
 
-    omp_sched_t loop_schema = omp_sched_dynamic;
+    omp_sched_t loop_schema = get_schema_of_loop();
     int chunks = 4;
     omp_set_schedule(loop_schema,chunks);
     /*
@@ -38,7 +42,7 @@ int main(int argc, char **argv)
         Chunks is amount of subgroups...
     */
 
-    #pragma omp parallel for schedule(static, 4) reduction(+ : sum)
+    #pragma omp parallel for reduction(+ : sum)
     for(i=0;i<num_steps;i++){
         sum += 4.0/(1.0+((i+0.5)*step)*((i+0.5)*step));
     }
@@ -74,3 +78,55 @@ int main(int argc, char **argv)
 
     printf("\nResult for parraled code with %c schema is: %f and it was counted in %f seconds.\n",(char)used_schema_type,pi,end_time-start_time);
 } 
+
+omp_sched_t get_schema_of_loop()
+{
+    int user_pick = 0;
+
+    while(user_pick<1 || user_pick>4)
+    {
+        system("@cls||clear");
+
+        printf("\nPick schema type: \n  ");
+        printf("    1) static");
+        printf("    2) dynamic");
+        printf("    3) guided");
+        printf("    4) auto\n\n");
+
+        printf("Your pick: ");
+        scanf("%d",&user_pick);
+
+        printf("\n1 flaga\n");
+
+        if (user_pick<1 || user_pick>4)
+        {
+            printf("You were supposed to pick number 1-4 !");
+            printf("Press Any Key to Continue\n");  
+            printf("\n2 flaga\n");
+            getchar();    
+        }  
+    }
+
+    switch(user_pick) 
+        {
+            case 1:
+                printf("\n3 flaga\n");
+                return omp_sched_static;
+                break;
+            case 2: 
+                printf("\n3 flaga\n");
+                return omp_sched_dynamic;
+                break;
+            case 3: 
+                printf("\n3 flaga\n");
+                return omp_sched_guided;
+                break;
+            case 4: 
+                printf("\n3 flaga\n");
+                return omp_sched_static;
+                break;
+            default:
+                system("pause");
+                break;
+        }
+}
