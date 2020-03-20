45ee51cc6c7ad515f7c1f0c5c5c6eab40a320bbd
marcinbogiel <marcin.bogiel@gmail.com>
2020-03-20 15:51:39 +0100

    Code format

--- My solutions/exercise_4_2.c
+++ My solutions/exercise_4_2.c
@@ -4,20 +4,28 @@
 #include <string.h>
 
 /*
+    The exercise was about experimenting with paralleled
+    program by trying out different loop schemas and chunks.
+
+    I decided to completely rebuild my exercise_4.c solutions
+    to make it fully configurable(and train my C skills too ;). 
+    Right now it's possible to experiment without recompiling it 
+    every time when there is a need to change loop schema or chunks amout.
+
     You can find the video with given exercise here:
-    
+    https://www.youtube.com/watch?v=8jzHiYo49G0#t=4m55s
 */
   
 static long num_steps = 100000;
 double step; 
 
-omp_sched_t get_schema_of_loop();
-int get_amount_of(char *picked_item);
-const char * set_used_schema_type(omp_sched_t loop_schema);
 void configure_parallel_environment();
 void set_parallel_environment();
 void calculate_the_integral();
 void display_summary();
+omp_sched_t get_schema_of_loop();
+int get_amount_of(char *picked_item);
+const char * set_used_schema_type(omp_sched_t loop_schema);
 
 int threads_amount;
 omp_sched_t loop_schema;
@@ -33,6 +41,44 @@ int main(int argc, char *argv)
     calculate_the_integral();
     display_summary();
 } 
+
+void configure_parallel_environment()
+{
+    threads_amount = get_amount_of("threads");
+    loop_schema = get_schema_of_loop();
+    chunks_amount = get_amount_of("chunks");
+}
+
+void set_parallel_environment()
+{
+    omp_set_num_threads(threads_amount);
+    omp_set_schedule(loop_schema,chunks_amount);
+}
+
+void calculate_the_integral()
+{
+    double sum = 0.0;
+    int i;
+    step = 1.0/(double) num_steps;
+
+    start_time = omp_get_wtime();
+    #pragma omp parallel for reduction(+ : sum)
+    for(i=0;i<num_steps;i++){
+        sum += 4.0/(1.0+((i+0.5)*step)*((i+0.5)*step));
+    }
+    program_result = step * sum;
+    end_time = omp_get_wtime();
+}
+
+void display_summary()
+{
+    omp_get_schedule(&loop_schema,&chunks_amount);  
+
+    strcpy(str_used_schema_type,set_used_schema_type(loop_schema));
+
+    printf("\nResult  is: %f. \nAnd it was counted in %f seconds.\n",program_result,end_time-start_time);
+    printf("With %d threads, %s schema and %d chunks. \n\n",threads_amount,str_used_schema_type,chunks_amount);
+}
 
 omp_sched_t get_schema_of_loop()
 {
@@ -111,42 +157,4 @@ const char * set_used_schema_type(omp_sched_t loop_schema)
             return "not defined";
             break;
     }
-}
-
-void configure_parallel_environment()
-{
-    threads_amount = get_amount_of("threads");
-    loop_schema = get_schema_of_loop();
-    chunks_amount = get_amount_of("chunks");
-}
-
-void set_parallel_environment()
-{
-    omp_set_num_threads(threads_amount);
-    omp_set_schedule(loop_schema,chunks_amount);
-}
-
-void calculate_the_integral()
-{
-    double sum = 0.0;
-    int i;
-    step = 1.0/(double) num_steps;
-
-    start_time = omp_get_wtime();
-    #pragma omp parallel for reduction(+ : sum)
-    for(i=0;i<num_steps;i++){
-        sum += 4.0/(1.0+((i+0.5)*step)*((i+0.5)*step));
-    }
-    program_result = step * sum;
-    end_time = omp_get_wtime();
-}
-
-void display_summary()
-{
-    omp_get_schedule(&loop_schema,&chunks_amount);  
-
-    strcpy(str_used_schema_type,set_used_schema_type(loop_schema));
-
-    printf("\nResult  is: %f. \nAnd it was counted in %f seconds.\n",program_result,end_time-start_time);
-    printf("With %d threads, %s schema and %d chunks. \n\n",threads_amount,str_used_schema_type,chunks_amount);
 }
