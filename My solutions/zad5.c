20a59554495c3f9a781fddd9bafb0fa77182b800
marcinbogiel <marcin.bogiel@gmail.com>
2020-03-24 17:15:09 +0100

    complex name refactor

--- My solutions/zad5.c
+++ My solutions/zad5.c
@@ -13,21 +13,23 @@ struct d_complex{
 
 void testpoint(struct d_complex);
 
-struct d_complex c;
+
+
+struct d_complex complex;
 int numoutside = 0;
 
 int main(){
    int i, j;
    double area, error, eps  = 1.0e-5;
 
-#pragma omp parallel for default(shared) firstprivate(eps) private(c,j)
+#pragma omp parallel for default(shared) firstprivate(eps) private(complex,j)
     for (i=0; i<NPOINTS; i++) 
     {
         for (j=0; j<NPOINTS; j++) 
         {
-            c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
-            c.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
-            testpoint(c);
+            complex.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
+            complex.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
+            testpoint(complex);
         }
    }
    
@@ -35,18 +37,19 @@ int main(){
   error=area/(double)NPOINTS;
 
   printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
+  printf("Correct answer should be around 1.510659\n");
 }
 
-void testpoint(struct d_complex c)
+void testpoint(struct d_complex complex)
 {
     struct d_complex z;
     int iter;
     double temp;
 
-    z=c;
+    z=complex;
     for (iter=0; iter<MAXITER; iter++){
-        temp = (z.r*z.r)-(z.i*z.i)+c.r;
-        z.i = z.r*z.i*2+c.i;
+        temp = (z.r*z.r)-(z.i*z.i)+complex.r;
+        z.i = z.r*z.i*2+complex.i;
         z.r = temp;
 
         if ((z.r*z.r+z.i*z.i)>4.0) {
@@ -54,6 +57,6 @@ void testpoint(struct d_complex c)
            numoutside++;
            break;
         }
-    }
+  }
 }
 
